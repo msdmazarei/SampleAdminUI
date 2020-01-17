73d01f116c414609f7c567c4cb6d6990b5813047
hamidreza <hr.kazemi11@gmail.com>
2020-01-17 21:02:03 +0330

    index html & tsx updated

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './asset/style/app/skin-default/style.scss';
 import App from './component/layout/app/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('content'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
