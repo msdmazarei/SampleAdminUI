b6501bc4d525b31eed63a8b83c8fdc091fc9c724
hamidreza <hr.kazemi11@gmail.com>
2020-01-18 10:55:23 +0330

    redux persist added

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './asset/style/app/skin-default/style.scss';
-import App from './component/layout/app/App';
+import { App } from './component/layout/app/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { persistor, Store2 } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
-ReactDOM.render(<App />, document.getElementById('content'));
+ReactDOM.render(
+    <Provider store={Store2}>
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>
+    , document.getElementById('content')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
