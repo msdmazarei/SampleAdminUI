246532e0b210726b0bd2d62c98744e55122a4790
hamidreza <hr.kazemi11@gmail.com>
2020-01-20 18:56:00 +0330

    reducer theme added

--- src/redux/app_state.ts
+++ src/redux/app_state.ts
@@ -2,6 +2,7 @@ import { IUser } from '../model/model.user'
 import { TInternationalization } from '../config/setup';
 import { IToken } from '../model/model.token';
 import { NETWORK_STATUS } from '../enum/NetworkStatus';
+import { ITheme_schema } from './action/theme/themeAction';
 
 export interface redux_state {
     logged_in_user: IUser | null;
@@ -9,4 +10,5 @@ export interface redux_state {
     token: IToken;
     authentication: string;
     network_status: NETWORK_STATUS;
+    theme: ITheme_schema;
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -9,17 +9,20 @@ import { IToken } from '../model/model.token';
 import { reducer as TokenReducer } from './reducer/token';
 import { reducer as AuthenticationReducer } from './reducer/authentication';
 import { reducer as NetworkStatusReducer } from './reducer/network-status';
+import { reducer as ThemeReducer } from './reducer/theme';
 //
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import { NETWORK_STATUS } from '../enum/NetworkStatus';
+import { ITheme_schema } from './action/theme/themeAction';
 
-const reducers: ReducersMapObject<redux_state, AnyAction> = { // Action
+const reducers: ReducersMapObject<redux_state, AnyAction> = {
   logged_in_user: UserReducer as Reducer<IUser | null, AnyAction>,
   internationalization: InternationalizationReducer as Reducer<TInternationalization, AnyAction>,
   token: TokenReducer as Reducer<IToken, AnyAction>,
   authentication: AuthenticationReducer as Reducer<string, AnyAction>,
   network_status: NetworkStatusReducer as Reducer<NETWORK_STATUS, AnyAction>,
+  theme: ThemeReducer as Reducer<ITheme_schema, AnyAction>
 }
 
 const main_reducer = combineReducers(reducers);
